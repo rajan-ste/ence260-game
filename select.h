e5affb244a1c8dd8759fd2e817b54b3f627689b4
Katie Ryan <kry25@uclive.ac.nz>
2023-10-11 13:46:20 +1300

    new defines

---
+++ select.h
@@ -0,0 +1,16 @@
+/** @file select.h
+    @author Katie Ryan, Rajan Stephens
+    @date 11/10/2023
+    @brief select and shoot behaviour module
+    last edited 
+*/
+
+#ifndef SELECT_H
+#define SELECT_H
+
+#include "system.h"
+
+#define LEFT -1
+#define RIGHT 1
+#define LEFT_WALL 0
+#define RIGHT_WALL 6
