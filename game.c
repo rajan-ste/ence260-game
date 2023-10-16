88486080b28e21fad4c04f365fde895170be0d6f
Rajan Stephens <rst114@uclive.ac.nz>
2023-10-16 22:29:55 +1300

    finish slecet mode

--- game.c
+++ game.c
@@ -7,6 +7,7 @@
 
 #define START 0
 #define SELECT 1
+#define FINAL 2
 #define PACER_RATE 500
 #define TINYGL_RATE 500
 
@@ -15,10 +16,12 @@ static uint8_t curr_select = PAPER;
 
 typedef struct {
     uint8_t mode;
+    uint8_t action;
 } state_t;
 
 state_t state = {
-    START
+    START,
+    PAPER
 };
 
 static void start_menu(state_t* state) 
@@ -44,7 +47,6 @@ static void start_menu(state_t* state)
                 state->mode = SELECT;
                 step_text("");
                 tinygl_clear();
-                
             }
         }
     }
@@ -58,26 +60,50 @@ static void move_selector(state_t* state, uint8_t* curr_select)
 {
     navswitch_update();
     tinygl_update();
-    
-    
-    // change to wait state after turn taken
-    /* if(navswitch_push_event_p(NAVSWITCH_PUSH)) {
-        
-    } */
+   
 
-    // move selecter right
+    static uint8_t count = 0;
+    static char curr_state = 'P';
+
+    // display P and set current action to PAPER
+    if (navswitch_push_event_p(NAVSWITCH_NORTH) || count == 0) {
+        curr_state = 'P';
+        state->action = PAPER;
+        display_character(curr_state);
+        count++;
+    }
+
+     // display S and set current action to SCISSORS
     if (navswitch_push_event_p(NAVSWITCH_EAST)) {
-        display_character('P');
-        // select_move_right(curr_select);
-    }
-    // move selecter left
-    if (navswitch_push_event_p(NAVSWITCH_WEST)) {
-        display_character('R');
+        curr_state = 'S';
+        state->action = SCISSORS;
+        display_character(curr_state);
     }
 
+
+    // display R and set current action to ROCK
     if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-        display_character('S');
+        curr_state = 'R';
+        state->action = ROCK;
+        display_character(curr_state);
     }
+
+
+    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+        ir_uart_putc(curr_state);
+        state->mode = FINAL;
+    }
+}
+
+void result_mode(state_t* state)
+{
+     if (ir_uart_read_ready_p ())
+        {
+            char newchar = ir_uart_getc();
+            if (newchar == 'P' || newchar == 'S' || newchar == 'R') {
+	            check who won() ;
+            }
+        }
 }
 
 
@@ -105,6 +131,9 @@ int main (void)
             case SELECT :
                 move_selector(&state, &curr_select);
                 break;
+            case FINAL :
+                display_character('W');
+                break;    
         }
     }
 }
