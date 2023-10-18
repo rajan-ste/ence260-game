7319c7a080ff9596a3822a8e40c77accf43ee9e6
Rajan Stephens <rst114@uclive.ac.nz>
2023-10-19 00:30:16 +1300

    wait for other controller to press button

--- communications.c
+++
@@ -1,8 +0,0 @@
-/** @file communications.c
-    @author Katie Ryan, Rajan Stephens
-    @date 11/10/2023
-    @brief IR communications
-    last edited 
-*/
-
-
--- communications.h
+++
@@ -1,7 +0,0 @@
-/** @file communications.h
-    @author Katie Ryan, Rajan Stephens
-    @date 11/10/2023
-    @brief IR communications
-    last edited 
-*/
-
