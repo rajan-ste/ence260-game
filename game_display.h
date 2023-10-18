66fb291c033e5e1aa552ae7ce34e7d97fae5aa91
Katie Ryan <kry25@uclive.ac.nz>
2023-10-19 12:30:29 +1300

    done

--- game_display.h
+++ game_display.h
@@ -1,9 +1,10 @@
 /** @file   game_display.h
     @author Rajan Stephens, Katie Ryan
     @date   11/10/23
-    @brief  LED matrix driver.
+    @brief  Display Text & Character
     last edited 19/10/23     
 */
+
 
 #ifndef DISPLAY_H
 #define DISPLAY_H
@@ -13,12 +14,21 @@
 #include "pio.h"
 #include "../../utils/tinygl.h"
 #include "../fonts/font3x5_1.h"
-#include "../fonts/font5x7_1.h"
 #include "display.h"
 
 
+/** Scroll text across the screen using tinygl
+    @param text, the text to scroll */
 void scroll_text (char* text);
+
+
+/** Step text on the screen
+    @param text, text to step */
 void step_text (char* text);
+
+
+/** Flash a single character onto the screen:
+    @param character, the character to display */
 void display_character (char character);
 
 
